228d3c7da321146b5585ba9997507d6a310e857c
yfwz100 <yfwz100@yeah.net>
2014-03-23 01:05:20 +0800

    Implement catalog routing.

--- src/route-catalog.js
+++ src/route-catalog.js
@@ -2,18 +2,53 @@
 'use strict';
 
 var router = module.exports = require('express').Router();
+var mw = require('./middleware');
+var model = require('neuola-data');
 
-router.route('/create').get(function (req, res) {
-}).post(function (req, res) {
+router.route('/create').get(mw.toPage('catalog-create')).post(function (req, res) {
+  var rawCatalog = req.param('catalog');
+  if (rawCatalog) {
+    var catalog = new model.Catalog(rawCatalog);
+    catalog.save(function (err, doc) {
+      if (err) {
+        res.render('error', err);
+      } else {
+        res.render('catalog-create-done', doc);
+      }
+    });
+  } else {
+    res.render('invalid');
+  }
 });
 
-router.route('/update/:article').all(function (req, res) {
-}).get(function (req, res) {
-}).post(function (req, res) {
+router.route('/update/:id').get(mw.toPage('catalog-update')).post(function (req, res) {
+  var id = req.param('id');
+  var rawCatalog = req.param('catalog');
+  if (rawCatalog) {
+    var q = model.Catalog.findByIdAndUpdate(id, rawCatalog);
+    q.exec().then(function (doc) {
+      res.render('catalog-update-done', doc);
+    }, res.render.bind(res, 'error'));
+  } else {
+    res.render('invalid');
+  }
 });
 
-router.route('/delete/:article').get(function (req, res) {
+router.route('/delete/:id').get(function (req, res) {
+  var id = req.param('id');
+  if (id) {
+    var q = model.Catalog.findByIdAndRemove(id);
+    q.exec().then(function (doc) {
+      res.render('catalog-delete-done', doc);
+    }, res.render.bind(res, 'error'));
+  } else {
+    res.render('invalid');
+  }
 });
 
 router.route('/browse').all(function (req, res) {
+  var q = model.Catalog.find();
+  q.exec().then(function (docs) {
+    res.render('catalog-list-latest', {catalogs: docs});
+  }, res.render.bind(res, 'error'));
 });
