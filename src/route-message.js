61302f451158172686d9367fd6641bccb6dd1a3f
yfwz100 <yfwz100@yeah.net>
2014-03-23 00:50:30 +0800

    Implement message routing.

--- src/route-message.js
+++ src/route-message.js
@@ -2,18 +2,55 @@
 'use strict';
 
 var router = module.exports = require('express').Router();
+var mw = require('./middleware');
+var model = require('neuola-data');
 
-router.route('/create').get(function (req, res) {
-}).post(function (req, res) {
+router.route('/create').get(mw.toPage('msg-create')).post(function (req, res) {
+  var rawMsg = req.param('msg');
+  rawMsg.from = req.user._id;
+  if (rawMsg) {
+    var msg = model.Message(rawMsg);
+    msg.save().exec().then(function (doc) {
+      req.render('msg-create-done', doc);
+    }, res.render.bind(res, 'error'));
+  } else {
+    res.render('invalid');
+  }
 });
 
-router.route('/update/:msg').all(function (req, res) {
-}).get(function (req, res) {
-}).post(function (req, res) {
+router.route('/update/:msg').get(mw.toPage('msg-update')).post(function (req, res) {
+  var rawMsg = req.param('msg');
+  var q = model.Message.findByIdAndUpdate(rawMsg._id, {content: rawMsg.content});
+  q.exec().then(function (doc) {
+    res.render('msg-update-done', doc);
+  }, res.render.bind(res, 'error'));
 });
 
-router.route('/delete/:msg').get(function (req, res) {
+router.route('/delete/:msg').all(function (req, res) {
+  var id = req.param('msg');
+  var q = model.Message.findByIdAndRemove(id);
+  q.exec().then(function (doc) {
+    res.render('msg-delete-done', doc);
+  }, res.render.bind(res, 'error'));
 });
 
 router.route('/browse').all(function (req, res) {
+  var q = model.Message.listLatestMessages(req.user._id);
+  q.exec().then(function (docs) {
+    res.render('msg-list-latest', docs);
+  }, res.render.bind(res, 'error'));
+});
+
+router.route('/received').all(function (req, res) {
+  var q = model.Message.listByReceiver(req.user._id);
+  q.exec().then(function (docs) {
+    res.render('msg-sent', {msgs: docs});
+  }, res.render.bind(res, 'error'));
+});
+
+router.route('/sent').all(function (req, res) {
+  var q = model.Message.listBySender(req.user._id);
+  q.exec().then(function (docs) {
+    res.render('msg-received', {msgs: docs});
+  }, res.render.bind(res, 'error'));
 });
