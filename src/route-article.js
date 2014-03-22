4de95865088c9d97237a81f27bb2df2a496a143f
yfwz100 <yfwz100@yeah.net>
2014-03-22 23:07:06 +0800

    Implement the article routing.

--- src/route-article.js
+++ src/route-article.js
@@ -2,23 +2,58 @@
 'use strict';
 
 var router = module.exports = require('express').Router();
+var mw = require('./middleware');
+var model = require('neuola-data');
 
-router.route('/create').get(function (req, res) {
-  // TODO view the create page.
-}).post(function (req, res) {
-  // TODO post the article.
+router.route('/create').get(mw.toPage('article-create')).post(function (req, res) {
+  var rawPost = req.param('post');
+  if (!rawPost) {
+    res.render('invalid');
+  } else {
+    var post = new model.Post(rawPost);
+    post.save(function (err, doc) {
+      if (err) {
+        res.render('error', err);
+      } else {
+        res.render('article-create-done', doc);
+      }
+    });
+  }
 });
 
-router.route('/delete/:article').post(function (req, res) {
-  // TODO delete the article
+router.route('/delete/:article').all(function (req, res) {
+  var id = req.param('id');
+  if (!id) {
+    res.render('invalid');
+  } else {
+    var q = model.Post.findByIdAndRemove(id);
+    q.exec().then(function (doc) {
+      res.render('article-del', doc);
+    }, res.render.bind(res, 'error'));
+  }
 });
 
-router.route('/update/:article').get(function (req, res) {
-  // TODO view the update page.
-}).post(function (req, res) {
-  // TODO update a specific article
+router.route('/update/:article').get(mw.toPage('article-update')).post(function (req, res) {
+  var rawPost = req.param('post');
+  if (!rawPost) {
+    res.render('invalid');
+  } else {
+    var q = model.Post.findbyIdAndUpdate(rawPost.id, rawPost);
+    q.exec().then(function (doc) {
+      res.render('article-update-done', doc);
+    }, res.render.bind(res, 'error'));
+  }
 });
 
 router.route('/browse/:tag').get(function (req, res) {
-  // Browse the articles, optionally by a tag.
+  var tagstr = req.params('tag'), tags;
+  if (tagstr) {
+    tags = tagstr.split(/,/);
+  } else {
+    tags = [];
+  }
+  var q = model.Post.listByTags(tags);
+  q.exec().then(function (docs) {
+    res.render('article-list-by-tags', {articles: docs});
+  }, res.render.bind(res, 'error'));
 });
