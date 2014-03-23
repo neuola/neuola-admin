f902ffee4b47ba2584f5d573453646274f85ca57
yfwz100 <yfwz100@yeah.net>
2014-03-23 20:58:20 +0800

    It seems to work fine now...

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var os = require('os');
 
-require('tingodb');
+require('tungus');
 var app = express();
 
 // Application configuration
@@ -12,21 +12,39 @@ app.set('env', 'development');
 app.set('view cache', false);
 app.set('json spaces', true);
 
-// Log all requests.
-app.use(require('./src/middleware').log);
-
-// Mount the neuola-admin module.
-app.use(require('./src')({
-  // it's buggy yet.
-  db: 'tingodb://' + require('path').join(os.tmpdir(), '/neuola-data-tingodb')
-}));
-
 // Only start listening on 3000 when this file is run directly.
 if (!module.parent) {
+  // Log all requests.
+  app.use(require('./src/middleware').log);
+
+  // Construct a test user...
+  app.use(function (req, res, next) {
+    req.user = {
+      _id: 'test',
+      name: 'Tester',
+      email: 'test@test.com'
+    };
+    next();
+  });
+
+  if (process.argv.length > 2) {
+    // Mount the neuola-admin module.
+    app.use(require('./src')({
+      db: process.argv[2]
+    }));
+  } else {
+    console.log('Plese specify a MongoDB-compatible connection URL.');
+    process.exit(-1);
+  }
+
   // Start an application running at port 3000.
   app.listen(3000);
 
   console.log('Server is running at 3000');
 } else {
-  module.exports = app;
+  module.exports = function (config) {
+    // Mount the neuola-admin module.
+    app.use(require('./src')(config));
+    return app;
+  };
 }
